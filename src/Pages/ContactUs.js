4542194edc4c87959d5dae5ab91b8342ddbb6f86
vicky <viclet45@gmail.com>
2024-07-03 16:31:51 +0000

    I enhanced the UI of the contact us form

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -6,17 +6,17 @@ function ContactUs() {
       <div className="row justify-content-center">
         <div className="col-md-6">
           <h2 className="text-center mb-4">Contact Us</h2>
-          <form>
+          <form className="contact-form">
             <div className="form-group">
-              <label htmlFor="name">Name:</label>
+              <label htmlFor="name">Name</label>
               <input type="text" className="form-control" id="name" />
             </div>
             <div className="form-group">
-              <label htmlFor="email">Email:</label>
+              <label htmlFor="email">Email</label>
               <input type="email" className="form-control" id="email" />
             </div>
             <div className="form-group">
-              <label htmlFor="message">Message:</label>
+              <label htmlFor="message">Message</label>
               <textarea className="form-control" id="message" rows="4"></textarea>
             </div>
             <button type="submit" className="btn btn-primary">Send</button>
@@ -28,3 +28,4 @@ function ContactUs() {
 }
 
 export default ContactUs;
+
